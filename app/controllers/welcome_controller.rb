fb47835958c35e6f36457ed3d665229f1584c119
jkrouse13 <jon.krouse@gmail.com>
2016-10-18 17:11:38 -0400

    now selects randomly for each paragraph needed.  added more paragraphs to choose from for each lipsum

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,44 @@
 class WelcomeController < ApplicationController
   def index
     @welcome_message = 'Salutations!'
+    # render plain: @welcome_message
   end
 
   def show
     @greetee = params[:name].capitalize
+    # render plain: "Greetings!  Welcome to rails #{@greetee}!!!"
   end
 
   def lorem
     @times = params[:pgraphs_needed].to_i
     lore = {}
     lore["standard"] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    @lorem = []
     if params[:lorem_type]
       if params[:lorem_type] == 'hipsum'
-        @lorem = Hipster.possabilities.sample
-      elsif params[:lorem_type] == 'samuel'
-        @lorem = Samuel.possabilities.sample
+        if params[:pgraphs_needed]
+          @times.times do
+           @lorem << Hipster.possabilities.sample
+         end
+        else
+          @lorem = Hipster.possabilities.sample
+        end
+       elsif params[:lorem_type] == 'samuel'
+         if params[:pgraphs_needed]
+           @times.times do
+            @lorem << Samuel.possabilities.sample
+          end
+         else
+           @lorem = Samuel.possabilities.sample
+         end
       elsif params[:lorem_type] == 'futurama'
-        @lorem = Futurama.possabilities.sample
+        if params[:pgraphs_needed]
+          @times.times do
+           @lorem << Futurama.possabilities.sample
+         end
+        else
+          @lorem = Futurama.possabilities.sample
+        end
       else
         @lorem = lore['standard']
       end
