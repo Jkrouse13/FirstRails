430005787edbc7e060c977950ea9468f53e6e955
jkrouse13 <jon.krouse@gmail.com>
2016-10-18 15:27:02 -0400

    random selection for hipster added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,5 @@
+
+
 class WelcomeController < ApplicationController
   def index
     @welcome_message = 'Salutations!'
@@ -18,7 +20,7 @@ class WelcomeController < ApplicationController
     lore["samuel"] =  "You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man."
     if params[:lorem_type]
       if params[:lorem_type] == 'hipsum'
-        @lorem = lore['hipsum']
+        @lorem = Hipster.possabilities.sample
       elsif params[:lorem_type] == 'samuel'
         @lorem = lore['samuel']
       elsif params[:lorem_type] == 'futurama'
